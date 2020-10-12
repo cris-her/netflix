7c7e4b0ec9f76f30dfcc6f88f319879698af8286
Emilio Castro <emilio.castro@mercadolibre.com>
2020-10-12 18:26:59 -0300

    Applying linter to project Finishing jumbotron styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import jumboData from './fixtures/jumbo.json'
-import Jumbotron from './components/jumbotron'
+import jumboData from './fixtures/jumbo.json';
+import Jumbotron from './components/jumbotron';
 
 export default function App() {
   return (
     <Jumbotron.Container>
-      {jumboData.map(item => (
+      {jumboData.map((item) => (
         <Jumbotron key={item.id} direction={item.direction}>
           <Jumbotron.Pane>
             <Jumbotron.Title>{item.title}</Jumbotron.Title>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import 'normalize.css';
 import App from './App';
-import { GlobalStyles } from './global-styles'
+import { GlobalStyles } from './global-styles';
 
 render(
   <React.StrictMode>
