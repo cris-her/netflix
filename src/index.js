1a2f9f3a841c43c60ce35e704aa187650fc9014c
Emilio Castro <emilio.castro@mercadolibre.com>
2020-10-12 17:57:35 -0300

    Adds jumbotron compound component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import { GlobalStyles } from './global-styles'
 
 render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
