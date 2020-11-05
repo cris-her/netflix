c31af0383d4d2603f4001402dad1c25ee671a1b1
Emilio Castro <emilio.castro@mercadolibre.com>
2020-11-05 20:14:41 -0300

    Adds Header to Home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { OptForm } from '../components';
 import { JumbotronContainer } from '../containers/jumbotron';
 import { FooterContainer } from '../containers/footer';
 import { FaqsContainer } from '../containers/faqs';
+import { HeaderContainer } from '../containers/header';
 
 export default function Home() {
   return (
     <>
+      <HeaderContainer>
+        <OptForm>
+          <OptForm.Input placeholder="Email address" />
+          <OptForm.Button>Try it now</OptForm.Button>
+          <OptForm.Break />
+          <OptForm.Text>Ready to watch? Enter your email to create or restart your membership</OptForm.Text>
+        </OptForm>
+      </HeaderContainer>
       <JumbotronContainer />
       <FaqsContainer />
       <FooterContainer />
