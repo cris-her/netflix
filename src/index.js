86898b9e5f2bfab1029f70313da60214528efd1f
Emilio Castro <emilio.castro@mercadolibre.com>
2020-11-06 21:14:20 -0300

    Adds React Context for Firebase

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import { render } from 'react-dom';
 import 'normalize.css';
 import App from './App';
 import { GlobalStyles } from './global-styles';
+import { FirebaseContext } from './context/firebase';
 import { firebase } from './lib/firebase.prod';
 
 render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <FirebaseContext.Provider value={{ firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
