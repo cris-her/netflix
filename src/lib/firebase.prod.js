06f651e672389d054fd547d32a0d56ce7b4dfb83
Emilio Castro <emilio.castro@mercadolibre.com>
2020-11-06 21:04:13 -0300

    Adds web app's Firebase configuration

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -5,7 +5,15 @@ import 'firebase/auth';
 // we need to somehow seed the database
 
 // we need a config here
-const config = {};
+const config = {
+  apiKey: 'AIzaSyD0oyI-pPWLL2W9xpVrwOb8pu_GRCPjiZw',
+  authDomain: 'netflix-clone-781ae.firebaseapp.com',
+  databaseURL: 'https://netflix-clone-781ae.firebaseio.com',
+  projectId: 'netflix-clone-781ae',
+  storageBucket: 'netflix-clone-781ae.appspot.com',
+  messagingSenderId: '246069506669',
+  appId: '1:246069506669:web:850262c347e8c6789d4564',
+};
 
 const firebase = Firebase.initializeApp(config);
 
