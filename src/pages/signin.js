e9cb5a26ddffd31726d3852049692036a8eace4d
Emilio Castro <emilio.castro@mercadolibre.com>
2020-11-11 18:48:07 -0300

    Adds Firebase top level authentication

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { FirebaseContext } from '../context/firebase';
 import { Form } from '../components';
 import { HeaderContainer } from '../containers/header';
 import { FooterContainer } from '../containers/footer';
+import * as ROUTES from '../constants/routes';
 
 export default function Signin() {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
   const [emailAddress, setEmailAddress] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
@@ -13,6 +18,17 @@ export default function Signin() {
     event.preventDefault();
 
     // firebase work here!
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(emailAddress, password)
+      .then(() => {
+        history.push(ROUTES.BROWSE);
+      })
+      .catch((error) => {
+        setEmailAddress('');
+        setPassword('');
+        setError(error.message);
+      })
   };
 
   return (
