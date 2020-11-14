3f2f07e22fcb8a39c8ac28c6e62590b56948209f
Emilio Castro <emilio.castro@mercadolibre.com>
2020-11-14 19:24:17 -0300

    Adds custom hook to handle authentication state changing

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Home, SignUp, SignIn, Browse } from './pages';
 import * as ROUTES from './constants/routes';
 import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
+import { useAuthListener } from './hooks';
 
 export default function App() {
-  const user = {};
+  const { user } = useAuthListener();
 
   return (
     <Router>
