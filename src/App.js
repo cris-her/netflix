ffac78b9532f72b2306fffe720aada518b8815e0
Emilio Castro <emilio.castro@mercadolibre.com>
2020-11-14 19:12:29 -0300

    Adds Protected Route helper to send users to the right page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Home, SignUp, SignIn, Browse } from './pages';
 import * as ROUTES from './constants/routes';
-import { IsUserRedirect } from './helpers/routes';
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
 
 export default function App() {
   const user = {};
@@ -15,12 +15,12 @@ export default function App() {
       <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN} exact>
         <SignIn />
       </IsUserRedirect>
-      <Route path={ROUTES.BROWSE}>
+      <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
         <Browse />
-      </Route>
-      <Route exact path={ROUTES.HOME}>
+      </ProtectedRoute>
+      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME} exact>
         <Home />
-      </Route>
+      </IsUserRedirect>
     </Router>
   );
 }
