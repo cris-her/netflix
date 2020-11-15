29590efd2d27e6b6452375aedb4058788c49f29d
Emilio Castro <emilio.castro@mercadolibre.com>
2020-11-15 18:43:00 -0300

    Adds firebase environment config

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -6,13 +6,13 @@ import 'firebase/auth';
 
 // we need a config here
 const config = {
-  apiKey: 'AIzaSyD0oyI-pPWLL2W9xpVrwOb8pu_GRCPjiZw',
-  authDomain: 'netflix-clone-781ae.firebaseapp.com',
-  databaseURL: 'https://netflix-clone-781ae.firebaseio.com',
-  projectId: 'netflix-clone-781ae',
-  storageBucket: 'netflix-clone-781ae.appspot.com',
-  messagingSenderId: '246069506669',
-  appId: '1:246069506669:web:850262c347e8c6789d4564',
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
 const firebase = Firebase.initializeApp(config);
