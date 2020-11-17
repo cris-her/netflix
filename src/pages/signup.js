7225f8b265d3f69641fa604f632047199db7ce57
Emilio Castro <emilio.castro@mercadolibre.com>
2020-11-17 19:41:58 -0300

    Adds tests to Sign In and Sign Up pages

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -65,7 +65,7 @@ export default function Signup() {
               autoComplete="off"
               onChange={({ target }) => setPassword(target.value)}
             />
-            <Form.Submit disabled={isInvalid} type="submit">
+            <Form.Submit disabled={isInvalid} type="submit" data-testid="sign-up">
               Sign Up
             </Form.Submit>
 
